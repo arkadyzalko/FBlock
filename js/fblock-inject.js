9e09363e828015e155db4b87655acc5d261aadba
Arkady Zalkowitsch <arkadyzalko@gmail.com>
2015-08-20 02:00:09 -0300

    - Hide Sidebar User - Search (Need to hide parent of element)

--- js/fblock-inject.js
+++ js/fblock-inject.js
@@ -1,9 +1,22 @@
-// Checking page title
+// Checking page
 if (document.getElementsByTagName("html")[0].id=="facebook") {
     //Creating Elements
     var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
+    var t = document.createTextNode("CLICK ME2");
     btn.appendChild(t);
     //Appending to DOM 
     document.body.appendChild(btn);
+
+
+    var xhr = new XMLHttpRequest();
+	xhr.open('GET', encodeURI('profile.php?id=848940441'));
+	xhr.onload = function() {
+	    if (xhr.status === 200) {
+	        console.log(xhr.responseURL);
+	    }
+	    else {
+	        alert('Request failed.  Returned status of ' + xhr.status);
+	    }
+	};
+	xhr.send();
 }
